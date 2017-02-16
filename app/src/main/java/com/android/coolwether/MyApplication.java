add2c4b1f32e9c73bd9bb8374a6ebf040d0f725e
利新 <200664516@qq.com>
2017-02-16 16:27:19 +0800

    加入创建数据库表和各项配置

---
+++ app/src/main/java/com/android/coolwether/MyApplication.java
@@ -0,0 +1,10 @@
+package com.android.coolwether;
+
+import org.litepal.LitePalApplication;
+
+/**
+ * Created by liulixin on 2017/2/16.
+ */
+
+public class MyApplication extends LitePalApplication {
+}
---
+++ app/src/main/java/com/android/coolwether/db/Country.java
@@ -0,0 +1,47 @@
+package com.android.coolwether.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by liulixin on 2017/2/16.
+ * 县 相关数据
+ */
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryName;
+    private int weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
